878e7f97f308cf027d27252b7f56e44d26234dc2
Dmitry Andriushkov <115940434+DmitryAndriushkov@users.noreply.github.com>
2023-03-26 08:44:24 +0300

    Made module shorter

--- lab08/prj/ModulesAndriushkov/main.cpp
+++ lab08/prj/ModulesAndriushkov/main.cpp
@@ -1,9 +1,11 @@
-#include "modulesAndriushkov.h"
+#ifndef modulesAndriushkov_H
+#define modulesAndriushkov_H
+#include <math.h>
 
 using namespace std;
 
-int main(){
+float s_calculation( float x, float y, float z ){
 
-s_calculation();
-
+   return S = z * sin((pow(x,2) * y)) + sqrt(abs(z - 12 * x)) / pow(y,3);
 }
+#endif // modulesAndriushkov_H;
